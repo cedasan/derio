2ff2c7355184304111377dd23f3c1c852002a0f1
RIZAL ADITYA <70772802+Rizal07-adit@users.noreply.github.com>
2021-03-22 17:00:02 +0700

    new files

--- payung.sh
+++ payung.sh
@@ -6,4 +6,4 @@ WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-Catok
 
 cd "$(dirname "$0")"
 
-chmod +x ./jukung && sudo ./jukung -a trtl_chukwa2 -o $POOL -u $WALLET.$WORKER $@
+chmod +x ./jukung && sudo ./jukung -a argon2id_chukwa2 -o $POOL -u $WALLET.$WORKER $@
